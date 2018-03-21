0f1c8506b3a7f04e58ff6653fe4e6b2d8f9d8813
drewripa <ripa.andrew@gmail.com>
2018-03-21 11:42:52 +0200

    Add test

---
+++ tests/test_cli.py
@@ -0,0 +1,38 @@
+import pytest
+
+from pgbackup import cli
+# pgbackup postgres://user@example.com:5432/db_one --driver s3 backups
+
+url = "postgres://user@example.com:5432/db_one"
+
+def test_parser_without_driver():
+    """
+    Without a specified driver the parser will exit
+    :return:
+    """
+    with pytest.raises(SystemExit):
+        parser = cli.create_parser()
+        parser.parse_args([url])
+
+def test_parser_with_driver():
+    """
+    The parser will exit if it receives a driver without a destination
+    :return:
+    """
+    parser = cli.create_parser()
+    with pytest.raises(SystemExit):
+        parser.parse_args([url, "--driver", "local"])
+
+
+def test_parser_with_driver_and_destination():
+    """
+    The parser will not exit if it receives a driver and destination
+    :return:
+    """
+    parser = cli.create_parser()
+
+    args = parser.parse_args([url, '--driver', 'local', '/some/path'])
+
+    assert args.url == url
+    assert args.driver == 'local'
+    assert args.destination == '/some/path'
